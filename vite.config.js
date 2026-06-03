fbe2ac9effb253d384b689324df52af07e460bda
Shiguretei-Amagasa <unusualize.com@gmail.com>
2026-06-04 08:31:10 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // GitHubリポジトリ名に合わせて変更してください
-// 例：リポジトリ名が "remnant-log" なら "/remnant-log/"
-const REPO_NAME = '/remnant-log/'
+// 例：リポジトリ名が "remnant_log" なら "/remnant_log/"
+const REPO_NAME = '/remnant_log/'
 
 export default defineConfig({
   plugins: [react()],
